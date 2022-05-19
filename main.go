00193960ea314565b94868531a7c72600e93ad17
Allan Davis <cajun.code@gmail.com>
2022-05-19 10:31:51 -0600

    finished generation

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"bytes"
+	"path/filepath"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
 )
 
 const (
@@ -34,4 +40,33 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func run(filename string) error {
+	input, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	htmlData := parseContent(input)
+
+	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
+	fmt.Println(outName)
+	return saveHTML(outName, htmlData)
+}
+
+func parseContent(input []byte) []byte {
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	var buffer bytes.Buffer
+	buffer.WriteString(header)
+	buffer.Write(body)
+	buffer.WriteString(footer)
+
+	return buffer.Bytes()
+}
+
+func saveHTML(outFname string, data []byte) error {
+	return os.WriteFile(outFname, data, 0644)
 }
